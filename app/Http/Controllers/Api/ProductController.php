6df373c5e27f7458139238b1499ed41dbb4f3186
diegocar448 <diegocar448@hotmail.com>
2018-09-21 15:55:40 -0300

    upload/arquivo produtos

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -27,7 +27,29 @@ class ProductController extends Controller
    
     public function store(StoreUpdateProductFormRequest $request)
     {
-        $product = $this->product->create($request->all());
+        $data = $request->all();
+
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+            //A função  kebab_case faz o tratamento de caracteres especiais
+            $name = Kebab_case($request->name);
+            //pegar a extensão do arquivo
+            $extension = $request->image->extension();
+            //Pegar o nome do arquvo e concatena com a extensão
+            $nameFile = "{$name}.{$extension}";
+
+            $data['image'] = $nameFile;
+
+            //iniciar o upload
+            $upload = $request->image->storeAs('products', $nameFile);
+
+
+            if(!$upload)
+            {
+                return response()->json(['error' => 'Fail_Upload'], 500);
+            }            
+        }
+
+        $product = $this->product->create($data);
 
         return response()->json($product, 201);
     }
